698b0f609c5270941450caba31954641e99d0456
Kasra Amini <kasraay98@gmail.com>
2023-02-15 21:04:55 -0500

    added hardware test

--- raspberry_pi/test_hardware.py
+++ raspberry_pi/test_hardware.py
@@ -1,9 +1,14 @@
 from pyembedded.raspberry_pi_tools.raspberrypi import PI
+import csv
 
 pi = PI()
-print(pi.get_ram_info())
+pi.get_ram_info()
 
-print(pi.get_cpu_usage())
+pi.get_cpu_usage()
+pi.get_cpu_temp()
 
-print(pi.get_cpu_temp())
+with open('usage.csv', 'w', newline='') as file:
+     writer = csv.writer(file)
 
+     writer.writerow(["ram", "cpu", "temp"])
+     writer.writerow([pi.get_ram_info(), pi.get_cpu_usage(), pi.get_cpu_temp()])
