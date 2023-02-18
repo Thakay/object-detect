83a9acd2847fd3e047ccf0a4bed67e17f9be6d2f
Kasra Amini <kasraay98@gmail.com>
2023-02-17 22:24:31 -0500

    detect edit

--- Coral/opencv/detect.py
+++ Coral/opencv/detect.py
@@ -37,6 +37,8 @@ from pycoral.adapters.detect import get_objects
 from pycoral.utils.dataset import read_label_file
 from pycoral.utils.edgetpu import make_interpreter
 from pycoral.utils.edgetpu import run_inference
+from pyembedded.raspberry_pi_tools.raspberrypi import PI
+import csv
 
 def main():
     default_model_dir = '../all_models'
@@ -70,7 +72,7 @@ def main():
     #cap = cv2.VideoCapture(args.camera_idx)
     imW = video.get(cv2.CAP_PROP_FRAME_WIDTH)
     imH = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
-    
+    pi = PI()
     row_size = 20  # pixels
     left_margin = 24  # pixels
     text_color = (0, 0, 255)  # red
@@ -87,58 +89,62 @@ def main():
     vidname = vidname.split('/')[-1]
     if not os.path.exists(f'./res/{checkname}{vidname}'):
       os.mkdir(f'./res/{checkname}{vidname}')
-    
-    while video.isOpened():
-        
-        ret, frame = video.read()
-        if not ret:
-            break
-        cv2_im = frame
-
-        cv2_im_rgb = cv2.cvtColor(cv2_im, cv2.COLOR_BGR2RGB)
-        cv2_im_rgb = cv2.resize(cv2_im_rgb, inference_size)
-        run_inference(interpreter, cv2_im_rgb.tobytes())
-        objs = get_objects(interpreter, args.threshold)[:args.top_k]
-        cv2_im = append_objs_to_img(cv2_im, inference_size, objs, labels)
-        results = append_objs_to_results(cv2_im, inference_size, objs, labels,results,counter)
-        
-        counter += 1
-        # Calculate the FPS
-        if counter % fps_avg_frame_count == 0:
-          end_time = time.time()
-          fps = fps_avg_frame_count / (end_time - start_time)
-          start_time = time.time()
-
-        # Show the FPS
-        fps_text = 'FPS = {:.1f}'.format(fps)
-        text_location = (left_margin, row_size)
-        cv2.putText(cv2_im, fps_text, text_location, cv2.FONT_HERSHEY_PLAIN,
-                    font_size, text_color, font_thickness)
-        if counter % 10 == 0:
-            
-            filename  = f'./res/{modelname}{vidname}/frame{counter}.jpg'
-            cv2.imwrite(filename, cv2_im)
-            aux_data = {}
-            for cnt,det in enumerate(objs):
-                
-                cat_name = labels.get(det.id)
-                aux_data['b_box'] = det.bbox
-                aux_data['frame_num'] = counter
-                aux_data.setdefault(cat_name,0)
-                aux_data[cat_name] += 1
-            data.append(aux_data)
-        
-        #cv2.imshow('frame', cv2_im)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    
-
-    with open(f"./res/results_{checkname}{vidname.split('/')[-1]}.csv","w", newline="") as file:
+    with open('usage.csv', 'a', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(["frameID","name","xmin","ymin","xmax","ymax"])
-        for i in results:
-            for j in results[i]:
-                writer.writerow([i,*j])
+
+        writer.writerow(["ram_used","ram_ava", "cpu", "temp"])
+        while video.isOpened():
+            ram = pi.get_ram_info()
+            writer.writerow([ram[1],ram[2], pi.get_cpu_usage(), pi.get_cpu_temp()])
+            ret, frame = video.read()
+            if not ret:
+                break
+            cv2_im = frame
+
+            cv2_im_rgb = cv2.cvtColor(cv2_im, cv2.COLOR_BGR2RGB)
+            cv2_im_rgb = cv2.resize(cv2_im_rgb, inference_size)
+            run_inference(interpreter, cv2_im_rgb.tobytes())
+            objs = get_objects(interpreter, args.threshold)[:args.top_k]
+            cv2_im = append_objs_to_img(cv2_im, inference_size, objs, labels)
+            results = append_objs_to_results(cv2_im, inference_size, objs, labels,results,counter)
+            
+            counter += 1
+            # Calculate the FPS
+            if counter % fps_avg_frame_count == 0:
+                end_time = time.time()
+                fps = fps_avg_frame_count / (end_time - start_time)
+                start_time = time.time()
+
+            # Show the FPS
+            fps_text = 'FPS = {:.1f}'.format(fps)
+            text_location = (left_margin, row_size)
+            cv2.putText(cv2_im, fps_text, text_location, cv2.FONT_HERSHEY_PLAIN,
+                        font_size, text_color, font_thickness)
+            # if counter % 10 == 0:
+                
+            #     filename  = f'./res/{modelname}{vidname}/frame{counter}.jpg'
+            #     cv2.imwrite(filename, cv2_im)
+            #     aux_data = {}
+            #     for cnt,det in enumerate(objs):
+                    
+            #         cat_name = labels.get(det.id)
+            #         aux_data['b_box'] = det.bbox
+            #         aux_data['frame_num'] = counter
+            #         aux_data.setdefault(cat_name,0)
+            #         aux_data[cat_name] += 1
+            #     data.append(aux_data)
+            
+            #cv2.imshow('frame', cv2_im)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        
+
+        # with open(f"./res/results_{checkname}{vidname.split('/')[-1]}.csv","w", newline="") as file:
+        #     writer = csv.writer(file)
+        #     writer.writerow(["frameID","name","xmin","ymin","xmax","ymax"])
+        #     for i in results:
+        #         for j in results[i]:
+        #             writer.writerow([i,*j])
     '''with open(f'./res/{checkname}{vidname}/res.txt','w') as f:
       f.write(pprint.pformat(data))
     pprint.pprint(data)
